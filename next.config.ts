2f2d5ebee8c53adc6430b4b415547e6980e82e80
Danila Vier <viierdanylo@gmail.com>
2026-09-23 19:01:14 +0300

    Add redirect from /admin to /user

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/admin/:path*',
+        destination: '/user/:path*',
+        permanent: true,
+      },
+      {
+        source: '/admin',
+        destination: '/user',
+        permanent: true,
+      }
+    ];
+  },
 };
 
 export default nextConfig;
